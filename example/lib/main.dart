1a79f891dd91c9d1a20aa2b117124701af070a41
hkloudou <hkloudou@gmail.com>
2021-05-05 11:06:28 +0800

    fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,38 +19,46 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    // initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterRichappbar.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
+  // // Platform messages are asynchronous, so we initialize in an async method.
+  // Future<void> initPlatformState() async {
+  //   String platformVersion;
+  //   // Platform messages may fail, so we use a try/catch PlatformException.
+  //   try {
+  //     platformVersion = await FlutterRichappbar.platformVersion;
+  //   } on PlatformException {
+  //     platformVersion = 'Failed to get platform version.';
+  //   }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+  //   // If the widget was removed from the tree while the asynchronous platform
+  //   // message was in flight, we want to discard the reply rather than calling
+  //   // setState to update our non-existent appearance.
+  //   if (!mounted) return;
 
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+  //   setState(() {
+  //     _platformVersion = platformVersion;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          foregroundColor: Color(0xFF606266),
+          backgroundColor: Colors.white,
+          brightness: Brightness.light,
+          backwardsCompatibility: false,
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+      ),
+      home: RichAppBarPage(
+        title: "动态标题",
+        body: Container(
+          height: 200,
+          color: Colors.red,
+          child: Text("测试"),
         ),
       ),
     );
--- lib/flutter_richappbar.dart
+++ lib/flutter_richappbar.dart
@@ -3,16 +3,162 @@
 // To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
 // directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
 
-import 'dart:async';
+import 'dart:math';
 
-import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 
-class FlutterRichappbar {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_richappbar');
+extension WidgetExtension on Widget {
+  Widget _refresh({Key? key, Future<void> Function()? onRefresh}) =>
+      onRefresh == null
+          ? this
+          : RefreshIndicator(
+              key: key,
+              onRefresh: onRefresh,
+              child: this,
+            );
+}
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+class _appbarBottom extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final double titleHeight;
+  _appbarBottom({
+    Key? key,
+    required this.titleHeight,
+    required this.title,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: EdgeInsets.only(left: 15),
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize {
+    return new Size.fromHeight(20.0);
+  }
+}
+
+class RichAppBarPage extends StatefulWidget {
+  final double titleHeight;
+  final double titleHeightPos;
+  final Widget? bottom;
+  final Widget? body;
+  final Widget? leading;
+  final String title;
+  final List<Widget>? actions;
+  final Future<void> Function()? onRefresh;
+  RichAppBarPage({
+    Key? key,
+    this.titleHeight = 40,
+    this.titleHeightPos = 30,
+    this.bottom,
+    this.leading,
+    this.title = "",
+    this.body,
+    this.onRefresh,
+    this.actions,
+  }) : super(key: key);
+  @override
+  State<StatefulWidget> createState() {
+    return _RichAppBarPageState();
+  }
+}
+
+class _RichAppBarPageState extends State<RichAppBarPage> {
+  GlobalKey<RefreshIndicatorState> _refreshKey =
+      GlobalKey<RefreshIndicatorState>();
+  ScrollController _scrollController = ScrollController();
+  double _op = 0;
+  late double _shouldHeight;
+  @override
+  void initState() {
+    super.initState();
+    _shouldHeight = widget.titleHeight;
+    // WidgetBuilder(
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      if (widget.onRefresh != null) {
+        _refreshKey.currentState?.show();
+      }
+      _scrollController
+        ..addListener(() {
+          setState(() {
+            _shouldHeight = widget.titleHeight -
+                max(
+                    min(_scrollController.offset - widget.titleHeight,
+                        widget.titleHeight),
+                    0);
+            // _op = (max(
+            //         min(_scrollController.offset - widget.titleHeight,
+            //             widget.titleHeightPos),
+            //         0) /
+            //     widget.titleHeightPos);
+          });
+        });
+    });
+  }
+
+  @override
+  void dispose() {
+    // print("dispose");
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: widget.leading,
+        actions: widget.actions,
+        title: Opacity(
+          opacity: _op,
+          child: Text(
+            widget.title,
+          ),
+        ),
+        elevation: 0,
+        bottom: _appbarBottom(
+          title: widget.title,
+          titleHeight: _shouldHeight,
+        ),
+        backwardsCompatibility: false,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  controller: _scrollController,
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: constraints.maxHeight +
+                          widget.titleHeight +
+                          widget.titleHeightPos,
+                      maxHeight: double.infinity,
+                    ),
+                    child: widget.body != null ? widget.body! : Container(),
+                  ),
+                );
+              },
+            ),
+          ),
+          widget.bottom != null ? widget.bottom! : Container(),
+        ],
+      ),
+    );
   }
 }
